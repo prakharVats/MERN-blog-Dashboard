4eefa4a2cf3a1eb00e54f66a308057945c57ba7f
prakharVats <vatsprakhar91@gmail.com>
2024-10-06 03:05:47 +0530

    wokring on nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,16 @@ import Signin from "./Pages/Signin"
 import Signup from "./Pages/Signup"
 import Dashboard from "./Pages/Dashboard"
 import Projects from "./Pages/Projects"
+import Header from "./Components/Header"
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path = "/" element = {<Home/>}/>
         <Route path = "/about" element = {<About/>}/>
         <Route path = "/sign-in" element = {<Signin/>}/>
-        <Route path = "/sign-up" element = {<Signup/>}/>
+        <Route path = "/sign-Sup" element = {<Signup/>}/>
         <Route path = "/dashboard" element = {<Dashboard/>}/>
         <Route path = "/projects" element = {<Projects/>}/>
       </Routes>
