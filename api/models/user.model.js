7a6888cdaa744e114d7ad861d45e5562274a6e5c
prakharVats <vatsprakhar91@gmail.com>
2024-10-06 18:03:00 +0530

    user schema complete

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+// at this point I have no idea what this schema thing is doing!!
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique: true,
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    password : {
+        type : String,
+        required : true
+    },
+} , {timestamps : true}
+)
+
+const user = mongoose.model("User" , userSchema);
+
+export default User;
