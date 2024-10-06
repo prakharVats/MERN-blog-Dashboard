57fc5485c89f3c706f0e2c992c3e81e731ac127c
prakharVats <vatsprakhar91@gmail.com>
2024-10-06 18:19:39 +0530

    test api and routes

--- api/index.js
+++ api/index.js
@@ -1,15 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
 import  dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js"
+const app = express();
 
+
+
+// database connection
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("connected");
 }).catch(err => {
     console.log("error connecting to the data base");
 });
-const app = express();
+
+// middleware
+app.use("/api/user" , userRoutes)
+
 
 app.listen(3000 , () => {
-    console.log("port 3000")
+    console.log("port 3000");
 })
